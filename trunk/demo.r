88e20d7913a63d6b0dcdf1a59b442c669399d752
Waclaw Kusnierczyk <waclaw.kusnierczyk@gmail.com>
2009-02-25 12:18:36 +0000

    added example 4 to demo.r

--- trunk/demo.r
+++ trunk/demo.r
@@ -3,10 +3,12 @@
 # loading benchmark directly from googlecode
 source('http://rbenchmark.googlecode.com/svn/trunk/benchmark.r')
 
+# example 1
 # benchmark the allocation of one 10^6-element numeric vector, replicated 100 times
 result = benchmark(1:10^6)
 print(result)
 
+# example 2
 # benchmark the application of two functions with like functionality but different implementation
 means.rep = function(n, m) mean(replicate(n, rnorm(m)))
 means.pat = function(n, m) colMeans(array(rnorm(n*m), c(m, n)))
@@ -17,6 +19,7 @@ result = benchmark(
    order=c('replications', 'elapsed'))
 print(result)
    
+# example 3
 # six benchmarks for means.pat(100, 100), each with 100 replications
 means.pat = function(n, m) colMeans(array(rnorm(n*m), c(m, n)))
 result = within(
@@ -26,4 +29,15 @@ result = within(
       means.pat(100, 100), 
       columns=c('test', 'elapsed', 'replications')),
    {average=elapsed/replications})
+print(result)
+
+# example 4
+# application of benchmark to a list of arbitrary expressions
+means.rep = function(n, m) mean(replicate(n, rnorm(m)))
+means.pat = function(n, m) colMeans(array(rnorm(n*m), c(m, n)))
+tests = list(
+   rep=expression(means.rep(100, 100)),
+   pat=expression(means.pat(100, 100)))
+result = do.call(benchmark, 
+   c(tests, list(replications=100, columns=c('test', 'elapsed', 'replications'), order='elapsed')))
 print(result)

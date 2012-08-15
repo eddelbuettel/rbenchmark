03bdfb4aec506178ff1279f5887a32461742d984
Berend Hasselman <bhh@xs4all.nl>
2012-08-15 17:59:26 +0000

    Modified benchmark.R to work with changed rep-function in R-patched and R-devel. rep() no longer accepts non-vector objects such as function calls. So now we do our own rep with a for-loop. \dontrun{} in .Rd file removed.

--- trunk/rbenchmark/R/benchmark.R
+++ trunk/rbenchmark/R/benchmark.R
@@ -21,7 +21,25 @@ function(
    for (i in 1:n$tests)
       if (is.expression(values[[i]])) 
          arguments[i] = values[i]
-   tests = rep(arguments, each=n$replications)
+
+   # this no longer works in R-2.15.1 patched and R-devel
+   # since rep can now only handle vector
+   # tests = rep(arguments, each=n$replications)
+
+   # so we have to copy arguments in an ugly way 
+   # if replications is > 1 extend tests by
+   # copying each argument entry to tests  n$replications consecutive times
+   # this extends tests
+   # initialize tests with arguments with everything R needs
+   tests <- arguments
+   if( n$replications > 1 ) {
+       z <- 1
+       for(k in 1:length(arguments)) {
+           tests[z:(z+n$replications-1)] <- arguments[k]
+           z <- z+n$replications
+       }
+   }
+
    result = data.frame(
       row.names=NULL,
       test=labels,

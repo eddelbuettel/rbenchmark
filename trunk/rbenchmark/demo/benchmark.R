1853f1ac95d2269af5478c6af702cbd10750f4e3
Waclaw Kusnierczyk <waclaw.kusnierczyk@gmail.com>
2012-08-30 10:00:02 +0000

    Polished code and documentation, updated repository site to be in sync with the package documentation.  Updated examples for more clarity

--- trunk/rbenchmark/demo/benchmark.R
+++ trunk/rbenchmark/demo/benchmark.R
@@ -1,38 +1,41 @@
 library(rbenchmark)
 
 # Example 1
-# Benchmark the allocation of one 10^6-element numeric vector,
-# replicated 100 times
+# Benchmarking the allocation of one 10^6-element numeric vector,
+# by default replicated 100 times
 benchmark(1:10^6)
 
+# simple test functions used in subsequent examples
+random.array = function(rows, cols, dist=rnorm) 
+                  array(dist(rows*cols), c(rows, cols))
+random.replicate = function(rows, cols, dist=rnorm)
+                      replicate(cols, dist(rows))
+
 # Example 2
-# A call to benchmark with two named expressions and three replication
-# counts, output sorted by the replication counts and then by the elapsed time:
-means.rep = function(n, m) mean(replicate(n, rnorm(m)))
-means.pat = function(n, m) colMeans(array(rnorm(n*m), c(m, n)))
-benchmark(rep=means.rep(100, 100),
-          pat=means.pat(100, 100),
-          replications=10^(1:3),
-          order=c('replications', 'elapsed'))
+# Benchmarking an expression multiple times with the same replication count,
+# output with selected columns only
+benchmark(replications=rep(100, 3),
+          random.array(100, 100),
+          random.array(100, 100),
+          columns=c('test', 'elapsed', 'replications'))
 
 # Example 3
-# A call to benchmark with duplicate expressions and replication counts,
-# output with selected columns, additional column computed afterwards:
-means.pat = function(n, m) colMeans(array(rnorm(n*m), c(m, n)))
-within(benchmark(replications=rep(100, 3),
-                 means.pat(100, 100),
-                 means.pat(100, 100),
-                 columns=c('test', 'elapsed', 'replications')), 
-       {average=elapsed/replications})
+# Benchmarking two named expressions with three different replication
+# counts, output sorted by test name and replication count,
+# with additional column added after the benchmark
+within(benchmark(rep=random.replicate(100, 100),
+                 arr=random.array(100, 100),
+                 replications=10^(1:3),
+                 columns=c('test', 'replications', 'elapsed'),
+                 order=c('test', 'replications')),
+       { average = elapsed/replications })
 
 # Example 4
-# A call to benchmark with a list of arbitrary predefined expressions:
-means.rep = function(n, m) mean(replicate(n, rnorm(m)))
-means.pat = function(n, m) colMeans(array(rnorm(n*m), c(m, n)))
-tests = list(rep=expression(means.rep(100, 100)), pat=expression(means.pat(100, 100)))
+# Benchmarking a list of arbitrary predefined expressions
+tests = list(rep=expression(random.replicate(100, 100)), 
+             arr=expression(random.array(100, 100)))
 do.call(benchmark,
         c(tests, list(replications=100,
                       columns=c('test', 'elapsed', 'replications'),
                       order='elapsed')))
-
 
